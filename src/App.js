fa630301ee951ce98f7004c82721ed88660738d8
Nikola Mihajlovic <nikola.mihajlovic@ict.edu.rs>
2019-08-26 23:05:37 +0200

    Instaliran prettier

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <React.StrictMode>
+        <div>
+          <NavBar />
+        </div>
+      </React.StrictMode>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar = () => {
+  return <nav className="nav-bar"></nav>;
+};
+
+export default NavBar;
